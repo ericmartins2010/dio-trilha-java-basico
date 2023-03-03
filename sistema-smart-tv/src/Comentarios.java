8f7be16228a89d937a6a8a4c61fcf5366e874a14
ericmartins2010 <eric.martins2010@yahoo.com.br>
2023-03-03 19:20:32 -0300

    Exemplificando comentários simples e a nível doc.

---
+++ sistema-smart-tv/src/Comentarios.java
@@ -0,0 +1,21 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+         * Eu sou um comentário que posso ser mais detalhado
+         * quando necessário
+         */
+
+        
+    }
+    /**
+     * Estes dois asteriscos acima são para identificar que você
+     * pretende elaborar um comentário a nível de documentação.
+     * Que incrível!!!
+     */
+    public void metodo(){
+
+    }
+    
+}
